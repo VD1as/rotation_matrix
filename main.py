6e36c67ef2152415388f7558a4a1120731280463
Vinicius Dias <vdias.noite@gmail.com>
2024-08-11 16:54:26 -0300

    feat

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 choose_option = int(input('''select the option for the calculation: 
                             [1] - Calculation of inertial coordinates
                             [2] - Calculation of moving coordinates
-                            [3] - Identify rotation on a given axis
                             Choose: '''))
 
 coordinates_input = []
@@ -62,7 +61,7 @@ if choose_option == 1 or choose_option == 2:
             print(f" X = {result_matrix[0][0]:.6f} \n Y = {result_matrix[1][0]:.6f}")
 
     elif choose_option == 2:
-        
+
         if coordinates_input[0] == 0:
             print("Rotação no eixo X")
 
@@ -79,9 +78,7 @@ if choose_option == 1 or choose_option == 2:
 
             coordinates_matrix = np.array([[coordinates_input[0]],[coordinates_input[2]]])
 
-            moving_coordinates_matrix = np.array([[coordinates_input[0]],coordinates_input[2]])
-
-            result_matrix = np.dot(matrix_rotation_y,moving_coordinates_matrix)
+            result_matrix = np.dot(matrix_rotation_y,coordinates_matrix)
             print(f" X = {result_matrix[0][0]:.6f} \n Y = {result_matrix[1][0]:.6f}")
 
 
@@ -89,34 +86,9 @@ if choose_option == 1 or choose_option == 2:
             print("Rotação no eixo Z")
 
             coordinates_matrix = np.array([[coordinates_input[0]],[coordinates_input[1]]])
-
-            moving_coordinates_matrix = np.array([[coordinates_input[0]],[coordinates_input[1]]])
         
-            result_matrix = np.dot(matrix_rotation_z,moving_coordinates_matrix)
+            result_matrix = np.dot(matrix_rotation_z,coordinates_matrix)
             print(f" X = {result_matrix[0][0]:.6f} \n Y = {result_matrix[1][0]:.6f}")
 
-elif choose_option == 3:
-    matrix_numbers = []
-    for i in range(1,10):
-        numbers = int(input("Insert the numbers for the matrix: "))
-        matrix_numbers.append(numbers)
-
-    rotation_matrix = [
-            [matrix_numbers[0],matrix_numbers[1],matrix_numbers[2]],
-            [matrix_numbers[3],matrix_numbers[4],matrix_numbers[5]],
-            [matrix_numbers[6],matrix_numbers[7],matrix_numbers[8]]]
-
-    print(rotation_matrix)
-
-    def check_matrix(matrix):
-        flat_list = [item for sublist in matrix for item in sublist]
-        zero_count = flat_list.count(0)
-        one_count = flat_list.count(1)
-        
-        if zero_count == 4 and one_count == 1 and len(flat_list) == zero_count + one_count + flat_list.count(flat_list[flat_list.index(1) + 1]):
-            return True
-        return False
-
-    final_rotation_matrix = check_matrix(rotation_matrix)
-
-    print(final_rotation_matrix)
+    else:
+        print("Choose an valid calculation run")
